c2b02d35a6225be0254ec4b494bd867c6be48351
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-09-25 11:26:14 +0300

    fixed filter props

--- src/components/Phonebook/Filter.jsx
+++ src/components/Phonebook/Filter.jsx
@@ -19,6 +19,11 @@ export default function Filter({ filter, onChange }) {
   );
 }
 
+Filter.defaultProps = {
+  filter: '',
+};
+///Якщо вказати filter: PropTypes.string.isRequired, то в консолі видає Warning: Failed prop type: The prop `filter` is marked as required in `Filter`, but its value is `undefined`.
+///Однак, якщо поставити Filter.defaultProps, то помилка не виникає, це нормальна практика, чи "костилі"?
 Filter.propTypes = {
   filter: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
